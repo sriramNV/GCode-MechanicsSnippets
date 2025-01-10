56efe9f936f07ee0d98029f12b416d0cbae2db5d
sriram N V <sriramnvilvanathan@gmail.com>
2025-01-10 17:11:08 +0530

    Updated Basic window with OOPs(implemented Classes)

--- Python/BasicWindow.py
+++ Python/BasicWindow.py
@@ -1,18 +1,24 @@
 import pygame
 import sys
 
-pygame.init()
 
-pygame.display.set_caption('Untitled')
-screen = pygame.display.set_mode((640,480))
+class Game:
+    def __init__(self):
+        pygame.init()
 
-clock = pygame.time.Clock()
+        pygame.display.set_caption('Untitled')
+        self.screen = pygame.display.set_mode((640,480))
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    
-    pygame.display.update()
-    clock.tick(60)
+        self.clock = pygame.time.Clock()
+
+    def run():
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            pygame.display.update()
+            clock.tick(60)
+
+Game().run()
