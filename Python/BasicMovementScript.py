8639659929fe9fbde7537aa4aeee541c29bdcf3d
sriram N V <sriramnvilvanathan@gmail.com>
2025-01-13 21:01:56 +0530

    added side movement(left and right) controls

--- Python/BasicMovementScript.py
+++ Python/BasicMovementScript.py
@@ -11,17 +11,18 @@ class Game:
 
         self.clock = pygame.time.Clock()
 
-        self.img = pygame.image.load('Python/image.png')
+        self.img = pygame.image.load('image.png')
         self.img.set_colorkey((0, 0, 0)) # for transparent background of images
 
         self.img_pos = [160, 260]
-        self.movement = [False, False]
+        self.movement = [False, False, False, False]
 
 
 
     def run(self):
         while True:
             self.screen.fill((14,219,248))
+            self.img_pos[0] += (self.movement[3] - self.movement[2]) * 5 
             self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5  # bool are converted to 0 or 1, true = 1, false = 0. 1 - 0 = 1, 1 - 1 = 0
             self.screen.blit(self.img, self.img_pos)
 
@@ -34,11 +35,19 @@ class Game:
                         self.movement[0] = True
                     if event.key == pygame.K_DOWN or event.key == pygame.K_s:
                         self.movement[1] = True
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                        self.movement[2] = True
+                    if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                        self.movement[3] = True
                 if event.type == pygame.KEYUP:
                     if event.key == pygame.K_UP or event.key == pygame.K_w:
                         self.movement[0] = False
                     if event.key == pygame.K_DOWN or event.key == pygame.K_s:
                         self.movement[1] = False
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_a:
+                        self.movement[2] = False
+                    if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
+                        self.movement[3] = False
 
             if self.movement[0]:
                 self.img_pos[1] -= 1
